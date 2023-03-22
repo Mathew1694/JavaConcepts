16524a138634f7a1886567e614361650b18bd83a
mathew1694 <cecilmattheww@gmail.com>
2023-03-22 21:07:37 +0530

    list & map logic

---
+++ src/test/java/com/sample/tests/TwoList.java
@@ -0,0 +1,40 @@
+package com.sample.tests;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class TwoList {
+
+	public static String a[] = { "Red", "Tea", "Coding", "Hat", "Fence", "Lavender", "Ranch", "cat", "apple",
+			"Apricot" };
+
+	public static void main(String[] args) {
+
+		List<String> strList = new ArrayList<>(Arrays.asList(a));
+		Collections.sort(strList);
+
+		Map<Integer, String> em = new HashMap<>();
+		for (int i =0 ; i < strList.size(); i++) {
+			em.put(i+1, strList.get(i));
+		}
+
+		System.out.println(em);
+
+		
+		Set<Entry<Integer, String>> x = em.entrySet();
+		
+		for (Entry<Integer, String> ent : x) {
+			if (ent.getValue().equalsIgnoreCase("Hat")) {
+				System.out.println("val is --->>> "+ent.getKey());
+			}
+		}
+
+	}
+
+}
